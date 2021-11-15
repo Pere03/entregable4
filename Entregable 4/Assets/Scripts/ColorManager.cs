71b7843b9d07df2857405dc6724e264505b8361b
Pere03 <91455056+Pere03@users.noreply.github.com>
2021-11-15 21:13:56 +0100

    v1

--- Entregable 4/Assets/Scripts/ColorManager.cs
+++ Entregable 4/Assets/Scripts/ColorManager.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class ColorManager : MonoBehaviour
 {
     public Color RandomColor;
+    public Vector3 RandomScale;
+    public Vector3 RandomPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+  
+    }
+
+    private void OnMouseDown()
+    {
+        //Cambiar el color cuando se pulse encima del objeto
+        RandomColor = new Color(Random.RandomRange(0f, 1f),
+        Random.RandomRange(0f, 1f), Random.RandomRange(0f, 1f));
+
+        GetComponent<MeshRenderer>().material.color = RandomColor;
+
+        //Cambiar la escala cuando se pulse encima del objeto
+        RandomScale = new Vector3(Random.RandomRange(-1f, 1f),
+        Random.RandomRange(-1f, 1f), Random.RandomRange(-1f, 1f));
+
+        transform.localScale = RandomScale;
+
+        //Cambiar la posicion cuando se pulse encima del objeto
+        RandomPosition = new Vector3(Random.RandomRange(-5f, 5f),
+        Random.RandomRange(-5f, 5f));
+
+        transform.position = RandomPosition;
+       
     }
 }
